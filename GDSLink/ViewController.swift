0551e11963a3fac5200a9be792c9c51d33d0a5a1
DevZaid <zaidameer72@gmail.com>
2016-08-28 09:51:54 +0300

    Initial Commit

---
+++ GDSLink/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  GDSLink
+//
+//  Created by Zaid Ameer on 8/28/16.
+//  Copyright © 2016 Zaid Ameer. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
