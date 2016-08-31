234b8d956a8895c3cf84d8ee2c949542f49b3421
DevZaid <zaidameer72@gmail.com>
2016-08-31 12:05:05 +0300

    YESSSS

--- GDSLink/ViewController.swift
+++ GDSLink/ViewController.swift
@@ -13,7 +13,7 @@ import MediaPlayer
 
 class ViewController: UIViewController {
     
-     var linksA = [String]()
+    var linksA = [String]()
 
     var streamLink : String!
     
@@ -70,8 +70,6 @@ class ViewController: UIViewController {
         /// /[u]00../g
         
         
-        
-        
         let firstElement = linksA.first
     
         let t = firstElement!.stringByReplacingOccurrencesOfString(",35", withString: "")
@@ -82,19 +80,24 @@ class ViewController: UIViewController {
         
         // the link needs to be decoded
         
-        print("URL : \(link)")
+        let i = link.stringByAddingPercentEscapesUsingEncoding(NSUTF8StringEncoding)
+
+        
+        let p = i!.stringByReplacingOccurrencesOfString("%5Cu", withString: "")
+        
         
         // you can see how the link should look like here :
         // http://ddecode.com/hexdecoder/?results=d82d4e564eccc1a6b96ee7c5c1e1c3b2
         
-        print("---")
+        // %252C : ,
+        // 003d : =
+        // 0026 : &
         
+         let re = p.stringByReplacingOccurrencesOfString("003d", withString: "=")
+         let y = re.stringByReplacingOccurrencesOfString("0026", withString: "&")
+            let c = y.stringByReplacingOccurrencesOfString("%252C", withString: ",")
         
-    
-         let re = link.stringByReplacingOccurrencesOfString("003d", withString: "=")
-        let y = re.stringByReplacingOccurrencesOfString("0026", withString: "&")
-        
-        print(y)
+        print(c)
         
 //        let c = y.stringByAddingPercentEscapesUsingEncoding(NSUTF8StringEncoding)
 //        print(c)
@@ -112,20 +115,15 @@ class ViewController: UIViewController {
 //        
 //        print(regex.matchesInString(link, options: [], range: range))
         
-        // https://r6---sn-nv47ln7s.c.docs.google.com/videoplayback?requiressl\u003dyes\u0026id\u003ddec4b6a250e5365c\u0026itag\u003d22\u0026source\u003dwebdrive\u0026ttl\u003dtransient\u0026app\u003dtexmex\u0026ip\u003d185.143.40.72\u0026ipbits\u003d8\u0026expire\u003d1472558967\u0026sparams\u003drequiressl%2Cid%2Citag%2Csource%2Cttl%2Cip%2Cipbits%2Cexpire\u0026signature\u003d7205FE8A78B40C3A302903366174A161821EF50D.8B3B221DDFF8A05611000F83806A2B92B4ECA05F\u0026key\u003dck2\u0026mm\u003d30\u0026mn\u003dsn-nv47ln7s\u0026ms\u003dnxu\u0026mt\u003d1472544221\u0026mv\u003dm\u0026nh\u003dIgpwcjAyLnNvZjAxKgkxMjcuMC4wLjE\u0026pl\u003d24
-        
-        //https://r6---sn-nv47ln7s.c.docs.google.com/videoplayback?requiressl=yes&id=dec4b6a250e5365c&itag=22&source=webdrive&ttl=transient&app=texmex&ip=185.143.40.72&ipbits=8&expire=1472558967&sparams=requiressl,id,itag,source,ttl,ip,ipbits,expire&signature=7205FE8A78B40C3A302903366174A161821EF50D.8B3B221DDFF8A05611000F83806A2B92B4ECA05F&key=ck2&mm=30&mn=sn-nv47ln7s&ms=nxu&mt=1472544221&mv=m&nh=IgpwcjAyLnNvZjAxKgkxMjcuMC4wLjE&pl=24
-
-        // 003d = =
-        // 0026 = &
         
         
-        streamLink = link
         
-      //  playVideo()
+        
     }
     
     func playVideo() {
+        
+        print(streamLink)
         
         let url = NSURL(string: streamLink)!
         
@@ -143,4 +141,6 @@ class ViewController: UIViewController {
 
     
 }
+
+
 
